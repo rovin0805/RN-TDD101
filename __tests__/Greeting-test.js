0925ec3c864282f209809b53df82031899685f48
Choi Eun Jeong <qhdrn67@naver.com>
2022-05-05 15:26:01 +0900

    button component unit test

---
+++ __tests__/Greeting-test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react-native';
+import Greeting from '../src/Greeting';
+
+describe('[Greeting] Test', () => {
+  const onPressMock = jest.fn();
+  const props = {
+    title: 'Hello!',
+    onPress: onPressMock,
+  };
+
+  test('A button is pressed.', () => {
+    const rendered = render(<Greeting {...props} />);
+
+    for (let i = 0; i < 5; i++) {
+      fireEvent(rendered.getByText('Hello!'), 'onPress');
+    }
+
+    expect(onPressMock).toBeCalledTimes(5);
+    expect(rendered.toJSON().children[0].children[0].children[0]).toEqual(
+      'Hello!',
+    );
+  });
+});
